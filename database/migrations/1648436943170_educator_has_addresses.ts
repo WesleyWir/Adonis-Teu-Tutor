fd00b033db41bc3782880507975b929ee476ae40
Wesley Wirmond <wesleywirmond@gmail.com>
2022-06-17 14:16:08 -0300

    small migration adjustment

--- database/migrations/1648436943170_educator_has_addresses.ts
+++ database/migrations/1648436943170_educator_has_addresses.ts
@@ -7,7 +7,7 @@ export default class EducatorHasAddresses extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('educator_id').references('educators.id').onDelete('CASCADE')
-      table.integer('address_id').unsigned().references('educator_adresses.id').onDelete('CASCADE')
+      table.integer('address_id').unsigned().references('educator_addresses.id').onDelete('CASCADE')
       table.unique(['educator_id', 'address_id'])
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
